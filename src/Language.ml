c75731067dda598aa9d848017fef9bc7306a80b6
Simiyutin Boris <simiyutin.boris@yandex.ru>
2018-03-12 04:23:29 +0300

    hw1-2 code moved

--- src/Language.ml
+++ src/Language.ml
@@ -44,7 +44,29 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let b2i x = if x then 1 else 0
+    let i2b x = if x = 0 then false else true
+
+    let parseBinOp op z1 z2 = match op with
+        "+" -> z1 + z2
+      | "-" -> z1 - z2
+      | "*" -> z1 * z2
+      | "/" -> z1 / z2
+      | "%" -> z1 mod z2
+      | ">" -> b2i (z1 > z2)
+      | "<" -> b2i (z1 < z2)
+      | ">=" -> b2i (z1 >= z2)
+      | "<=" -> b2i (z1 <= z2)
+      | "==" -> b2i (z1 = z2)
+      | "!=" -> b2i (z1 <> z2)
+      | "!!" -> b2i ((i2b z1) || (i2b z2))
+      | "&&" -> b2i ((i2b z1) && (i2b z2))
+      | _ -> failwith ("unknown__operand:" ^ op)
+
+    let rec eval s e = match e with
+        Const(z) -> z
+       | Var(x) -> s x
+       | Binop(str, e1, e2) -> parseBinOp str (eval s e1) (eval s e2)
 
     (* Expression parser. You can use the following terminals:
 
@@ -78,7 +100,13 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval (s, i, o) stmt = match stmt with
+    | Read(x)       -> (match i with
+                       | hd::tl -> (Expr.update x hd s, tl, o)
+                       | _      -> failwith "trying to read from empty stream")
+    | Write(e)      -> (s, i, o @ [Expr.eval s e])
+    | Assign(x, e)  -> (Expr.update x (Expr.eval s e) s, i, o)
+    | Seq(st1, st2) -> eval (eval (s, i, o) st1) st2
 
     (* Statement parser *)
     ostap (
--- src/SM.ml
+++ src/SM.ml
@@ -24,7 +24,36 @@ type config = int list * Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let handleBinOp op (stack, conf) = match stack with 
+  | h::hs::hss -> ((Language.Expr.parseBinOp op h hs)::hss, conf)
+  | _          -> failwith "stack is too small"
+
+let handleConst i (stack, conf) = (i::stack, conf)
+
+let handleRead (stack, conf) = match conf with
+  | (s, h::hs, o) -> (h::stack, (s, hs, o))
+  | _             -> failwith "cannot read from empty input stream"
+
+let handleWrite (stack, (s, i, o)) = match stack with
+  | h::hs -> (hs, (s, i, o@[h]))
+  | _     -> failwith "stack is too small"
+
+let handleLoad x (stack, (s, i, o)) = (s x::stack, (s, i, o))
+
+let handleStore x (stack, (s, i, o)) = match stack with 
+  | h::hs -> (hs, (Language.Expr.update x h s, i, o))
+  | _     -> failwith "stack is too small"
+                  
+let rec eval conf prg = match prg with 
+  | [] -> conf
+  | hd::tl -> ( match hd with
+        | BINOP(op) -> eval (handleBinOp op conf) tl
+        | CONST(i)  -> eval (handleConst i conf) tl
+        | READ      -> eval (handleRead conf) tl
+        | WRITE     -> eval (handleWrite conf) tl
+        | LD(x)     -> eval (handleLoad x conf) tl
+        | ST(x)     -> eval (handleStore x conf) tl
+  )
 
 (* Top-level evaluation
 
@@ -41,6 +70,21 @@ let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+let rec compileExpr pg e = match e with
+         Language.Expr.Const(z)           -> pg@[CONST z]
+       | Language.Expr.Var(x)             -> pg@[LD x]
+       | Language.Expr.Binop(str, e1, e2) -> pg@(compileExpr [] e2)@(compileExpr [] e1)@[BINOP str]
+
+let rec compileStmt pg stmt = match stmt with
+   (* read to stack and store it to variable *)
+    | Language.Stmt.Read(x)       -> pg@[READ]@[ST x]
+   (* evaluate expression and write from stack *)
+    | Language.Stmt.Write(e)      -> pg@compileExpr [] e@[WRITE]
+   (* evaluate expression and store it to variable *)
+    | Language.Stmt.Assign(x, e)  -> pg@compileExpr [] e@[ST x]
+   (* concatenate programs recursively *)
+    | Language.Stmt.Seq(st1, st2) -> pg@compileStmt [] st1@compileStmt [] st2
+
+let compile stmt = compileStmt [] stmt
 
                          
